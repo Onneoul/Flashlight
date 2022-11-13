b98a401f3587abc502718b79adc72073fa2ac01b
onneoul <kimminse99@naver.com>
2022-11-14 01:30:20 +0900

    Complete Version

--- app/src/main/java/com/example/flashlight/MainActivity.java
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -3,12 +3,73 @@ package com.example.flashlight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int globalColorId = R.color.white;
+    private String bgColor = "white";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            bgColor = savedInstanceState.getString("bgColor");
+            globalColorId = savedInstanceState.getInt("colorId", globalColorId);
+        }
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("bgColor", bgColor);
+        savedInstanceState.putInt("colorId", globalColorId);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        changeColor(bgColor, globalColorId);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    public void onClickWhite(View view) {
+        Button button = findViewById(R.id.whiteButton);
+        changeColor((String) button.getContentDescription(), R.color.white);
+    }
+    public void onClickRed(View view) {
+        Button button = findViewById(R.id.redButton);
+        changeColor((String) button.getContentDescription(), R.color.red);
+    }
+    public void onClickBlue(View view) {
+        Button button = findViewById(R.id.blueButton);
+        changeColor((String) button.getContentDescription(), R.color.blue);
+    }
+    public void onClickGreen(View view) {
+        Button button = findViewById(R.id.greenButton);
+        changeColor((String) button.getContentDescription(), R.color.green);
+    }
+    public void onClickPurple(View view) {
+        Button button = findViewById(R.id.purpleButton);
+        changeColor((String) button.getContentDescription(), R.color.purple);
+    }
+
+    public void changeColor(String color, int colorId) {
+        final LinearLayout ly = findViewById(R.id.mainArea);
+        ly.setBackgroundColor(getResources().getColor(colorId));
+        bgColor = color;
+        globalColorId = colorId;
+//        Toast toast = Toast.makeText(this, bgColor, Toast.LENGTH_SHORT);
+//        toast.show(); // 테스트용 코드
     }
 }
